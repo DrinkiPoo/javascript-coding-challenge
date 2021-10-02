9e6f7375afe6942562a830c36a021a38eda7caa5
devesh lamichhane <deveshl@protonmail.com>
2021-10-03 01:45:00 +0545

    updated quick_sort.js

--- quick_sort.js
+++ quick_sort.js
@@ -1,27 +1,29 @@
-let sorted = [];
 function quickSort(array) {
-  if (array.length == 0) {
-    return;
-  } else if (array.length == 1) {
-    sorted.push(array[0]);
-    return;
-  }
-
-  let pivot = array[array.length - 1];
-  let big = [];
-  let small = [];
-  for (let i = 0; i < array.length - 2; i++) {
-    if (array[i] < pivot) {
-      small.push(array[i]);
-    } else if (array[i] >= pivot) {
-      big.push(array[i]);
+  let sorted = [];
+  const func = (array) => {
+    if (array.length == 0) {
+      return;
+    } else if (array.length == 1) {
+      sorted.push(array[0]);
+      return;
     }
-  }
-  quickSort(small);
-  sorted.push(pivot);
-  quickSort(big);
+
+    let pivot = array[array.length - 1];
+    let big = [];
+    let small = [];
+    for (let i = 0; i <= array.length - 2; i++) {
+      if (array[i] < pivot) {
+        small.push(array[i]);
+      } else if (array[i] >= pivot) {
+        big.push(array[i]);
+      }
+    }
+    func(small);
+    sorted.push(pivot);
+    func(big);
+  };
+  func(array);
+  return sorted;
 }
 
-quickSort([1, 4, 2, 8]);
-
-console.log(sorted);
+console.log(quickSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]));
